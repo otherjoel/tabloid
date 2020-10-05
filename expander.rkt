0c072c4436c8c05ae08ed689fb7353ca6960d6fa
Joel Dueck <joel@jdueck.net>
2020-10-05 12:20:27 -0500

    Implement multiply, divide and modulo, add more comments

--- expander.rkt
+++ expander.rkt
@@ -13,13 +13,17 @@
          block-scope
          variable-assign
          value-expression
+         product
          true
          false)
 
+;; Re-provide Racket’s default interposition points for use in #lang tabloid
+;; See https://beautifulracket.com/explainer/interposition-points.html
 (provide #%top #%app #%datum #%top-interaction)
 
 (define-macro tabloid-program #'#%module-begin)
 
+;; “Everything printed by Tabloid is automatically capitalized, and an exclamation point is added.”
 (define (print v)
   (define printable
     (cond
@@ -38,6 +42,13 @@
   [(_ LEFT "PLUS" RIGHT) #'(plus-things LEFT RIGHT)]
   [(_ LEFT "MINUS" RIGHT) #'(minus-things LEFT RIGHT)])
 
+(define-macro-cases product
+  [(_ VAL) #'VAL]
+  [(_ LEFT "TIMES" RIGHT) #'(multiply-things LEFT RIGHT)]
+  [(_ LEFT "DIVIDED BY" RIGHT) #'(divide-things LEFT RIGHT)]
+  [(_ LEFT "MODULO" RIGHT) #'(modulo-things LEFT RIGHT)])
+
+;; Coerce values in a way that roughly mimics Javascript
 (define (->str v)
   (cond
     [(string? v) v]
@@ -53,18 +64,25 @@
 
 (define (NAN? v) (equal? 'NAN v))
 
+;; If either operand is a string, treat both as strings and concatenate.
 (define (plus-things a b)
   (cond
     [(ormap string? (list a b)) (apply string-append (map ->str (list a b)))]
     [else (apply + (map ->number (list a b)))]))
 
-(define (minus-things a b)
-  (define operands (map ->number (list a b)))
-  (cond
-    [(ormap NAN? operands) 'NAN]
-    [else (apply - operands)]))
-
-;; TODO "TIMES" "DIVIDED BY" "MODULO"
+;; Build functions that perform binary operations on numbers only, returning NaN if either
+;; operand cannot be coerced into a number.
+(define (strict-math-op func)
+  (lambda (left right)
+    (define operands (map ->number (list left right)))
+    (cond
+      [(ormap NAN? operands) 'NAN]
+      [else (apply func operands)])))
+  
+(define minus-things (strict-math-op -))
+(define multiply-things (strict-math-op *))
+(define divide-things (strict-math-op /))
+(define modulo-things (strict-math-op modulo))
 
 (define-macro-cases conditional
   [(_ EVAL IF-TRUE) #'(cond [EVAL IF-TRUE])]
@@ -86,10 +104,12 @@
     [(ormap string? vals) (apply (dict-ref funcs `(string . ,op)) (map ->str vals))]
     [else (apply (dict-ref funcs `(other . ,op)) (map ->number vals))]))
 
-;; Tabloid functions can contain only one statement
+;; Tabloid functions can contain only one statement (but a block scope counts as one statement)
 (define-macro (function-def ID+ARGS EXPR)
   #'(define ID+ARGS EXPR))
 
+;; "SHOCKING DEVELOPMENT" is implemented as a continuation within a block scope
+;; See https://beautifulracket.com/explainer/continuations.html
 (define-macro (block-scope EXPR ...)
   (with-syntax ([shocking-return (datum->syntax caller-stx 'shocking-return)])
     #'(let/cc shocking-return

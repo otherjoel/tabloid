0b571a7d482e3aa000cdc28a369bef91149ee0a1
Joel Dueck <joel@jdueck.net>
2020-10-05 12:23:09 -0500

    Improve tokenizer and parser
    - SHOCKING DEVELOPMENT only allowed inside blocks or conditionals
    - Parser is appropriately greedy with expressions used as arguments in
    a function call

--- main.rkt
+++ main.rkt
@@ -1,13 +1,15 @@
 #lang br
 
 (require brag/support
-         "parser.rkt")
+         racket/match
+         "parser.rkt") ; Provides the `parse` function
 
 (define debug-mode? (make-parameter #f))
 
-;; This file includes the tokenizer/lexer and the reader.
+;; This file includes the tokenizer and the reader. The reader passes the token stream to the
+;; parser (see parser.rkt). The parser converts the tokens into an S-expression whose bindings are
+;; provided by the expander (see expander.rkt)
 
-;; 
 (define-lex-abbrev reserved-phrases
   (:or "YOU WON'T WANT TO MISS"
        "EXPERTS CLAIM"
@@ -16,20 +18,51 @@
        "RUMOR HAS IT" "END OF STORY"
        "DISCOVER HOW TO" "WITH"
        "SHOCKING DEVELOPMENT"
-       "IS ACTUALLY"
-       "OF"
-       "PLUS" "MINUS" "TIMES" "DIVIDED BY" "MODULO"
+       "PLEASE LIKE AND SUBSCRIBE"))
+
+;; These are in a separate group because they are allowed to appear in expressions that
+;; are passed as arguments to functions (see below)
+(define-lex-abbrev values-and-ops
+  (:or "PLUS" "MINUS" "TIMES" "DIVIDED BY" "MODULO"
        "AND" "OR"
+       "IS ACTUALLY"
        "TOTALLY RIGHT"
        "COMPLETELY WRONG"
        "BEATS"
-       "SMALLER THAN"
-       "SHOCKING DEVELOPMENT"
-       "PLEASE LIKE AND SUBSCRIBE"))
+       "SMALLER THAN"))
 
 (define-lex-abbrev matching-single-quotes (from/to "'" "'"))
 
-(define (make-tokenizer port)
+;; Identifiers can contain letters, numbers, and punctuation
+(define-lex-abbrev identifier-chars (:+ (:- (:or alphabetic punctuation numeric) (:or "(" ")" ","))))
+
+;; The hardest part of this project (so far) was getting the precedence right in the case where
+;; binary operations are used as in the last argument of a function application, e.g.:
+;;
+;;      YOU WON'T WANT TO MISS n TIMES function OF n PLUS 1
+;;
+;; This should be interpreted as (n TIMES (function OF n PLUS 1)) but I had a very difficult time
+;; getting the parser to do that instead of ((n TIMES (function OF n)) PLUS 1) and still hang on to
+;; operator precedence of TIMES over PLUS.
+;;   My solution is to use the tokenizer to insert a special token marking the end of an argument
+;; list, which in turn gives the parser a mechanism for being “greedy” with tokens in that context.
+;; The tokenizer can only return one token at a time, so when it’s in the middle of an argument list
+;; and the lexer encounters something that implicitly means the end of an argument list has been
+;; reached, it stores that thing in a parameter (`held-token`) and returns the END-ARGLIST token
+;; instead. The next time the tokenizer is called it will return (and clear out) the held token.
+
+(define in-arg-list? (make-parameter #f)) ; Keep track of whether we’re in an argument list
+(define held-token (make-parameter #f)) 
+
+(define (end-argument-list lex)
+  (cond [(in-arg-list?)
+         (held-token lex)
+         (in-arg-list? #f)
+         (token 'END-ARGLIST)]
+        [else lex]))
+
+;; Returns a function that spits out tokens one at a time.
+(define (make-tabloid-tokenizer port)
   (port-count-lines! port) ; Turn on source location tracking
   (lexer-file-path port)   ; Set location for error reporting
   
@@ -37,22 +70,33 @@
     (define tabloid-lexer
       (lexer-srcloc
        [whitespace (next-token)] ; “In Tabloid, newlines are not significant.”
-       [reserved-phrases lexeme]
+       [(eof) (end-argument-list lexeme)]
+       [reserved-phrases (end-argument-list lexeme)]
+       [values-and-ops lexeme]
        [(:or "(" ")" ",") lexeme]
 
        ;; Datum literals
        [matching-single-quotes (token 'STRING (trim-ends "'" lexeme "'"))]
        [(:: (:? "-") (:+ (char-set ".01234567890"))) (token 'NUMBER (string->number lexeme))]
 
-       ;; Identifiers can contain letters, numbers, and punctuation))
-       [(:: (:+ (:- (:or alphabetic punctuation numeric) (:or "(" ")" ","))))
+       ;; Function application
+       [(:: identifier-chars (:+ whitespace) "OF")
+        (begin (in-arg-list? #t)
+               (token 'APPLY-FUNC-ID (string->symbol (regexp-replace #rx"[ ]+OF" lexeme ""))))]
+
+       ;; Identifiers in any other context
+       [(:: identifier-chars)
         (token 'IDENTIFIER (string->symbol lexeme))]))
-    
-    (tabloid-lexer port))
+    (match (held-token)
+      [#f (tabloid-lexer port)]
+      [(var tok) (held-token #f) tok])) ; If we’re holding a token, return it and clear it out
   next-token)
 
+;; read-syntax is called by Racket itself as the first step in interpreting a #lang tabloid program
+;; See https://beautifulracket.com/appendix/master-recipe.html#the-reader
 (define (read-syntax src in-port)
-  (define parsed-tree (parse src (make-tokenizer in-port)))
+  ;; The “parse” function comes from parser.rkt
+  (define parsed-tree (parse src (make-tabloid-tokenizer in-port)))
   (define program
     (cond
       [(debug-mode?) parsed-tree]
--- parser.rkt
+++ parser.rkt
@@ -2,29 +2,32 @@
 
 ;; A #lang brag program automagically converts a BNF grammar (below) into a function
 ;; that parses bytes from an input port into an S-expression that matches the grammar.
-;; It provides that function under the name `parse`.
+;; It provides this function under the name `parse`.
 
 tabloid-program: statement+
 
-@statement: print | value-expression | variable-assign | function-def | program-end | shocking-return
+@statement: print | variable-assign | function-def
+            | conditional | block-scope | program-end
 
 print: /"YOU WON'T WANT TO MISS" value-expression
 
 variable-assign: /"EXPERTS CLAIM" IDENTIFIER /"TO BE" value-expression
 
-value-expression: [function-apply | value-expression ("PLUS" | "MINUS")]* (value | product | function-apply | conditional | block-scope | compare)
-
-product: [product ("TIMES" | "DIVIDED BY" | "MODULO")]* value
+value-expression: [value-expression ("PLUS" | "MINUS")]* (@value | product | function-apply)
+product: [product ("TIMES" | "DIVIDED BY" | "MODULO")]* value-expression
 
 @value: NUMBER | STRING | boolean | IDENTIFIER | /"(" value-expression /")"
 
-compare: value-expression ("IS ACTUALLY" | "BEATS" | "SMALLER THAN") value-expression
+function-apply: APPLY-FUNC-ID value-expression [/"," value-expression]* /END-ARGLIST
 
 @boolean: true | false
 true: /"TOTALLY RIGHT"
 false: /"COMPLETELY WRONG"
 
-conditional: /"WHAT IF" value-expression statement [/"LIES!" statement]
+conditional: /"WHAT IF" compare (statement | shocking-return)
+             [/"LIES!" (statement | shocking-return)]
+
+compare: value-expression ("IS ACTUALLY" | "BEATS" | "SMALLER THAN") value-expression
 
 function-def: /"DISCOVER HOW TO" id-and-arguments block-scope
 
@@ -33,9 +36,5 @@ function-def: /"DISCOVER HOW TO" id-and-arguments block-scope
 block-scope: /"RUMOR HAS IT" [statement | shocking-return]+ /"END OF STORY"
 
 shocking-return: /"SHOCKING DEVELOPMENT" value-expression
-
-function-apply: IDENTIFIER /"OF" argument-list
-
-@argument-list: [value-expression /","]* value-expression
 
 program-end: /"PLEASE LIKE AND SUBSCRIBE"
